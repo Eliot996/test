65ab76ea0572dc9934d0f38c642d2c47500403ba
Eliot996 <math10t6@stud.kea.dk>
2022-04-03 10:07:51 +0200

    wrong placement...

--- src/main/java/com/example/test/controllers/controller.java
+++ src/main/java/com/example/test/controllers/controller.java
@@ -1,4 +1,4 @@
-package controllers;
+package com.example.test.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class controller {
 
-    @GetMapping
+    @GetMapping("/")
     public String landing() {
-        return "landing";
+        return "Landing";
     }
 }
